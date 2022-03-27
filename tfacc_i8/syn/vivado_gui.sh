5b40174b3ac90ca3d012969a1e53369933c84bea
Shinichi Yamashita <s.yamashita@jcom.zaq.ne.jp>
2022-03-27 17:10:03 +0900

    add xilinx environ setting script

--- tfacc_i8/syn/vivado_gui.sh
+++ tfacc_i8/syn/vivado_gui.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#source /opt/Xilinx/Vivado/2014.4/settings32.sh
-#source /opt/Xilinx/Vivado/2016.4/settings64.sh
-#source /opt/Xilinx/Vivado/2019.2/settings64.sh
-source /opt/Xilinx/Vivado/2020.2/settings64.sh
+source xilinx_env.sh
 
 vivado
 
